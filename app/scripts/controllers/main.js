7114058cd49392427d6c978e48156d84915014c2
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-04-13 16:48:04 +0200

    Ahora se puede añadir nuevas tareas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,8 @@
 angular.module('tareasApp')
 .controller('MainCtrl',function($scope){
 	$scope.tareas= ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+	$scope.addTarea = function(){
+		$scope.tareas.push($scope.tarea);
+		$scope.tarea = '';
+	}
 });
